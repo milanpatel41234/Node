c57dec44d0c6d1c503f83e8303c29e1e89b3aa0b
milanpatel41234 <mpatel41234@gmail.com>
2023-08-21 11:36:53 +0530

    routing with express

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
 const http = require('http');
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
-app.use((req, res, next)=>{
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/addproduct',(req, res, next)=>{
+    res.send('<html><body><form action="/submitproduct" method="POST"><input type="text" name="title" placeholder="product name"><input type="text" name="size" placeholder="size"><button type="submit">Submit</button></form></body></html>');
+    
+})
+app.post('/submitproduct',(req, res, next)=>{
+    const body = req.body;
+    console.log(body);
+    res.redirect('/addproduct');
+})
+app.use('/',(req, res, next)=>{
     res.send('<h1>hello from express</h1>');
 })
 
